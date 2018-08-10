3ad98432b937a238971964ef176f8a5839998230
Michael Hu <pxmyhdev@gmail.com>
2018-08-09 23:23:30 -0400

    leetcode 268

---
+++ 101-200/268.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# 268. Missing Number
+# 
+# Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+# 
+# Example 1:
+# 
+# Input: [3,0,1]
+# Output: 2
+# Example 2:
+# 
+# Input: [9,6,4,2,3,5,7,0,1]
+# Output: 8
+# Note:
+# Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_num = max(nums)
+        for i in range (0, max_num+1):
+          if max_num-i not in nums and max_num-i != max_num:
+            return max_num - i
+        return max_num + 1
+          
