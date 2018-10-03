a4a34b5f3275db57b57c066f26da9dd4a485d2ea
Michael Hu <pxmyhdev@gmail.com>
2018-10-03 00:14:44 -0400

    add DoC 63 find word in matrix

---
+++ DoC/63.py
@@ -0,0 +1,28 @@
+# This problem was asked by Microsoft.
+
+# Given a 2D matrix of characters and a target word, write a function that returns whether the word can be found in the matrix by going left-to-right, or up-to-down.
+
+# For example, given the following matrix:
+
+# [['F', 'A', 'C', 'I'],
+#  ['O', 'B', 'Q', 'P'],
+#  ['A', 'N', 'O', 'B'],
+#  ['M', 'A', 'S', 'S']]
+# and the target word 'FOAM', you should return true, since it's the leftmost column. Similarly, given the target word 'MASS', you should return true, since it's the last row.
+
+
+# Method #1
+# The first method is straighforward, first check corner cases: if the given word is more than 4 characters long
+# then check if the first letter of the given word appears in the first line or first column
+# then repeat the process for all four characters
+
+def is_found(matrix, word):
+  if len(word)
+  return answer
+
+
+word_matrix = 
+
+# Method #2
+# convert letters in word into characters and convert characters into numbers
+# calculate the given word's sum of number representation of 
